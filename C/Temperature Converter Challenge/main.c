e977ce3952c5c098ad9748ff8aeee5c2c98113ba
Snipzwolf <jfcopsey@gmail.com>
2016-02-27 16:45:49 +0000

    added input validation and broke out into separate functions

--- C/Temperature Converter Challenge/main.c
+++ C/Temperature Converter Challenge/main.c
@@ -6,6 +6,8 @@
  */
 
 #include <stdio.h>
+#include <stdbool.h>
+#include "main.h"
 
 int main( void ){
 	int lower,
@@ -13,13 +15,41 @@ int main( void ){
 
 	setbuf(stdout, NULL); //disable stdout buffering as don't see prompts in eclipse
 
-	printf("Please insert a lower limit (value >= 0) [10]: ");
-	scanf("%d", &lower);
-	printf("Please insert a higher limit (value > 10 && value <= 50000) [50]: ");
-	scanf("%d", &higher);
+	do{
+		prompt(&lower, &higher);
+	}while(!validate(lower, higher));
 
 	printf("Lower limit is '%d'\n Higher limit is '%d", lower, higher);
 
-
 	return 0;
 }
+
+void prompt(int *lower, int *higher){
+	printf("Please insert a lower limit (value >= 0): ");
+	scanf("%d", lower);
+	printf("Please insert a higher limit (value > 10 && value <= 50000): ");
+	scanf("%d", higher);
+}
+
+bool validate(int lower, int higher){
+	bool ret = true;
+
+	if(lower < 0){
+		printf("ERROR lower limit < 0)\n");
+		ret = false;
+	}
+
+	if(higher < 10){
+		printf("ERROR higher limit < 10)\n");
+		ret = false;
+
+	}else if(higher > 50000){
+		printf("ERROR higher limit > 50000)\n");
+		ret = false;
+
+	}
+
+	return ret;
+}
+
+
---
+++ C/Temperature Converter Challenge/main.h
@@ -0,0 +1,15 @@
+/*
+ * main.h
+ *
+ *  Created on: 27 Feb 2016
+ *      Author: John
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+void prompt(int *lower, int *higher);
+bool validate(int lower, int higher);
+
+
+#endif /* MAIN_H_ */
