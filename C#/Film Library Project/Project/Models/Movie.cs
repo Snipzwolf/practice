04dc0cf56341dd6fa1e8d9de6a5758893c107406
John Copsey <Snipzwolf@users.noreply.github.com>
2016-03-05 19:04:35 +0000

    initial commit of Film Library Project

---
+++ C#/Film Library Project/Project/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Models
+{
+    class Movie
+    {
+        public int Id { get; set; }
+        public string ImdbId { get; set; }
+
+        public string Name { get; set; }
+        public DateTime Released { get; set; }
+        public TimeSpan RunTime { get; set; }
+        public HashSet<Genre> Genres { get; set; }
+    }
+}
---
+++ C#/Film Library Project/README.md
@@ -0,0 +1,3 @@
+# Film Library Project
+
+https://blog.udemy.com/c-sharp-projects
