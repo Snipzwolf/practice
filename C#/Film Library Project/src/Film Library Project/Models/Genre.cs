ed851039fcf7b9f310c417401601a364547c3a1b
John <john_copsey@idg.co.uk>
2017-04-21 12:44:48 +0100

    added genre model from previous version

---
+++ C#/Film Library Project/src/Film Library Project/Models/Genre.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace Film_Library_Project.Models
+{
+    [DataContract]
+    public class Genre
+    {
+        
+        public Genre() { }
+
+        [DataMember]
+        public int id { get; set; }
+
+        [DataMember]
+        public string name { get; set; }
+
+    }
+}
+}
