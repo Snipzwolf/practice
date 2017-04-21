7dc979e1b13e18ffad7c6bb3356616c79838261f
John <john_copsey@idg.co.uk>
2017-04-21 12:41:45 +0100

    added .net core project

---
+++ C#/Film Library Project/src/Film Library Project/Models/Movie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace Film_Library_Project.Models
+{
+    [DataContract]
+    public class Movie
+    {
+        public Movie() { }
+
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public string ImdbId { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public DateTime Released { get; set; }
+
+        [DataMember]
+        public TimeSpan RunTime { get; set; }
+
+        [DataMember]
+        public HashSet<Genre> Genres { get; set; }
+    }
+}
---
+++ C#/Film Library Project/src/Film Library Project/Services/OmdbApiService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Film_Library_Project.Services
+{
+    public class OmdbApiService
+    {
+        private readonly Uri apiUri;
+
+        public OmdbApiService()
+        {
+            apiUri = new UriBuilder("http", "www.omdbapi.com", 80).Uri;
+        }
+
+        public Movie getMovieByImdbId(string id)
+        {
+            HttpWebRequest request = getApiUri(new Dictionary<string, string>(){
+                { "i",  id },
+                { "type", "movie" },
+                { "plot", "full" },
+                { "r", "json" }
+            });
+        }
+
+        private HttpWebRequest getApiUri(Dictionary<String, String> qs)
+        {
+            UriBuilder requestUri = new UriBuilder(apiUri);
+
+            requestUri.Path = path;
+            if(qs.Count > 0)
+            {
+                StringBuilder sb = qs.Aggregate(new StringBuilder(), (a, b) => {
+                    if (a.Length > 0)
+                    {
+                        a.Append("&");
+                    }                        
+
+                    a.Append(b); //TODO maybe url encode??
+                    return a;
+                });
+
+                requestUri.Query = sb.ToString();
+            }
+
+            HttpWebRequest ret = WebRequest.CreateHttp(requestUri.Uri);
+
+            return ret;
+        }
+    }
+}
