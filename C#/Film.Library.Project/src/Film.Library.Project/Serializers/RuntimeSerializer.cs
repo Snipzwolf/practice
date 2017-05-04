f30a026d9af3818fa6655ab27430c719f44b7779
John <john_copsey@idg.co.uk>
2017-05-04 15:09:52 +0100

    finished implementation of RuntimeSerializer

--- C#/Film.Library.Project/src/Film.Library.Project/Serializers/RuntimeSerializer.cs
+++ C#/Film.Library.Project/src/Film.Library.Project/Serializers/RuntimeSerializer.cs
@@ -1,7 +1,9 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Film_Library_Project.Serializers
@@ -10,17 +12,36 @@ namespace Film_Library_Project.Serializers
     {
         public override bool CanConvert(Type objectType)
         {
-            throw new NotImplementedException();
+            return objectType == typeof(TimeSpan);
         }
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
-            throw new NotImplementedException();
+            if (reader.TokenType == JsonToken.Null)
+                return null;
+            /*
+             * http://stackoverflow.com/questions/8030538/how-to-implement-custom-jsonconverter-in-json-net-to-deserialize-a-list-of-base
+             * check this out if more complicated as it loading the reader like below
+             * doesn't copy over the full config
+             */
+            JToken jt = JToken.Load(reader);
+            String value = jt.Value<String>();
+
+            Regex rx = new Regex("(\\s*)min$");
+            value = rx.Replace(value, (m) => "");
+
+            int timespanMin; 
+            if(!Int32.TryParse(value, out timespanMin))
+            {
+                throw new NotSupportedException();
+            }
+
+            return new TimeSpan(0, timespanMin, 0);
         }
 
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
-            throw new NotImplementedException();
+            serializer.Serialize(writer, value);
         }
     }
 }
